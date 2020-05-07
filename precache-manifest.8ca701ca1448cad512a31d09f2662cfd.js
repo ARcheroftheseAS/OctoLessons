2f45dd5250eebbb350ca4a8d62baa2cd4c62fe27
ARcheroftheseAS <gzheng1026@gmail.com>
2020-05-07 13:34:48 -0700

    Updates

---
+++ precache-manifest.8ca701ca1448cad512a31d09f2662cfd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95d507005e92c95596b8d37e066fcaf1",
+    "url": "/OctoLessons/ReactApp/index.html"
+  },
+  {
+    "revision": "8ab19cf7a98fe8f3520c",
+    "url": "/OctoLessons/ReactApp/static/css/main.d81fccab.chunk.css"
+  },
+  {
+    "revision": "7bd32b0d0be5e2930973",
+    "url": "/OctoLessons/ReactApp/static/js/2.5dc8107f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/OctoLessons/ReactApp/static/js/2.5dc8107f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ab19cf7a98fe8f3520c",
+    "url": "/OctoLessons/ReactApp/static/js/main.617763b2.chunk.js"
+  },
+  {
+    "revision": "8b2ddb808e8bdfa650d6",
+    "url": "/OctoLessons/ReactApp/static/js/runtime-main.92d5ce57.js"
+  },
+  {
+    "revision": "56d1fa8e27c79d2380f64a45f6c5a174",
+    "url": "/OctoLessons/ReactApp/static/media/CyberGestalt.56d1fa8e.png"
+  },
+  {
+    "revision": "f3c1d11075b108e9a9b6d5b00705be9d",
+    "url": "/OctoLessons/ReactApp/static/media/Meme.f3c1d110.png"
+  },
+  {
+    "revision": "ba61eccde2eb63c3a61fa20b7fae4afd",
+    "url": "/OctoLessons/ReactApp/static/media/doodleKindness.ba61eccd.png"
+  },
+  {
+    "revision": "312390546ddd5e3dac64c4464d8429f3",
+    "url": "/OctoLessons/ReactApp/static/media/granadagbox.31239054.png"
+  },
+  {
+    "revision": "958904a89892f83b708953429c76334d",
+    "url": "/OctoLessons/ReactApp/static/media/iconB.958904a8.gif"
+  },
+  {
+    "revision": "b6f1c786dfcca8f50a1adbb9c7a856c8",
+    "url": "/OctoLessons/ReactApp/static/media/iconC.b6f1c786.gif"
+  },
+  {
+    "revision": "b66b5dd9e67c1456c5040c3c7c47cb72",
+    "url": "/OctoLessons/ReactApp/static/media/iconD.b66b5dd9.gif"
+  },
+  {
+    "revision": "d7dbe385bd20ee967ed696d6ff984461",
+    "url": "/OctoLessons/ReactApp/static/media/iconE.d7dbe385.gif"
+  },
+  {
+    "revision": "76eb56f0face25dbec10e4e64470ac8f",
+    "url": "/OctoLessons/ReactApp/static/media/iconF.76eb56f0.gif"
+  }
+]);
